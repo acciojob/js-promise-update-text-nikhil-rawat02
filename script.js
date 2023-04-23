3a03357a9b2b92b0819e5f127ad1e58cfcc21bc8
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-23 15:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let output = document.getElementById("output");
+
+let p = new Promise((resolve,reject) =>{
+	setInterval(resolve("change html"),1000);
+})
+p.then((value)=>{
+	output.innerHTML = value;
+})

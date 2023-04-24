5aa94a94a31674bea4d890b12bc645fdb855d9c1
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-25 00:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let output = document.getElementById("output");
 let p = new Promise((resolve,reject) =>{
-	setInterval(resolve("'Hello, world!'"),1000);
+	setInterval(resolve("Hello, world!"),1000);
 })
 p.then((value)=>{
 	output.innerHTML = value;
